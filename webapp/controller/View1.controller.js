576e570b90de484013964160093b99d11c3731da
Jared Leonard <jared.leonard@raymondcorp.com>
2021-08-30 17:16:44 +0000

    commit

---
+++ webapp/controller/View1.controller.js
@@ -0,0 +1,17 @@
+sap.ui.define([
+    "sap/ui/core/mvc/Controller",
+    "sap/m/MessageToast",
+	"project2/model/models",
+],
+	/**
+	 * @param {typeof sap.ui.core.mvc.Controller} Controller
+	 */
+	function (Controller, MessageToast) {
+		"use strict";
+
+		return Controller.extend("project2.controller.View1", {
+			onInit: function () {
+
+            }
+		});
+	});
